5a1157b00eb4f76f0bfbb86b5289c54543be3b62
Akinyele Cafe-Febrissy <me@akinyele.ca>
2021-05-12 21:53:47 -0400

    Implement login modal

---
+++ src/components/modals/LoginModal/LoginModal.tsx
@@ -0,0 +1,84 @@
+import {
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+import { useMemo, useState } from "react";
+import BasicModal from "../BasicModal";
+
+interface Props {
+  open: boolean;
+  onClose: VoidFunction;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      width: "100%",
+      margin: theme.spacing(1, 0),
+    },
+  },
+  inputIcon: {},
+  inputWrapper: {
+    flexBasis: 1,
+  },
+  fullWidth: {
+    width: "100%",
+  },
+}));
+
+export default function LoginModal({ open, onClose }: Props) {
+  const classes = useStyles();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validEntry = useMemo(
+    () =>
+      username.trim().length > 1 &&
+      username.trim().length < 64 &&
+      password.length >= 1,
+    [username, password]
+  );
+
+  return (
+    <BasicModal
+      title="Sign in"
+      description="Enter your MangaDex account credentials below."
+      open={open}
+      onClose={onClose}
+    >
+      <form noValidate autoComplete="off" className={classes.root}>
+        <FormControl>
+          <OutlinedInput
+            id="username"
+            type="username"
+            value={username}
+            placeholder="Your MangaDex username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <OutlinedInput
+            id="password"
+            type="password"
+            value={password}
+            placeholder="Your password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <Button
+            disabled={!validEntry}
+            size="large"
+            variant="contained"
+            color="primary"
+          >
+            Sign in
+          </Button>
+        </FormControl>
+      </form>
+    </BasicModal>
+  );
+}
--- src/config/providers/WithLayoutProvider/CustomAppBar.tsx
+++ src/config/providers/WithLayoutProvider/CustomAppBar.tsx
@@ -1,8 +1,12 @@
 import { Container, Grid, IconButton } from "@material-ui/core";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import { LoginModal } from "components/modals";
+import { useState } from "react";
 import JumpToMangaSearchField from "./JumpToMangaSearchField";
 
 export function CustomAppBar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <Container>
       <Grid container alignItems="flex-start">
@@ -16,11 +20,13 @@ export function CustomAppBar() {
             aria-controls="primary-search-account-menu"
             aria-haspopup="true"
             color="inherit"
+            onClick={() => setOpen(true)}
           >
             <AccountCircle />
           </IconButton>
         </Grid>
       </Grid>
+      <LoginModal open={open} onClose={() => setOpen(false)} />
     </Container>
   );
 }
